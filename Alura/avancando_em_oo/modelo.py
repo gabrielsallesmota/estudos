581f005e67a1596cc926e3a25b616276e9eccbb4
Gabriel Mota <gabrielsallesmota@hotmail.com>
2023-04-04 15:29:49 -0300

    Removendo Duplicações com Herança

--- Alura/avancando_em_oo/modelo.py
+++ Alura/avancando_em_oo/modelo.py
@@ -1,56 +1,44 @@
 # REFORÇANDO: Nome da classe sempre com a primeira maiúscula
-class Filme:
-    def __init__(self, nome, ano, duracao):
+class Programa:
+    def __init__(self, nome, ano):
         # .title() muda todas as primeiras para maiúscula
-        self.__nome = nome.title()
+        #um _ só ele nao muda o nome, porém ele também deixa ela protegida por convenção
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+print(vingadores.nome)
 
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-
-vingadores = Filme('vingadores', 2018, 160)
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Temporadas: {vingadores.duracao} - Likes {vingadores.likes}')
-
-atlanta = Serie('Atalanta', 2017, 2)
-atlanta.dar_like()
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
