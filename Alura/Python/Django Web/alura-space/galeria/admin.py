cd67ba6ebb52de32b5fec2f31b4f1bf043ded4d5
Gabriel Mota <gabrielsallesmota@hotmail.com>
2023-05-11 00:47:36 -0300

    CRUD Admin

--- Alura/Python/Django Web/alura-space/galeria/admin.py
+++ Alura/Python/Django Web/alura-space/galeria/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from galeria.views import Fotografia
 
-admin.site.register(Fotografia)
+from galeria.models import Fotografia
+
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+
+admin.site.register(Fotografia, ListandoFotografias)
