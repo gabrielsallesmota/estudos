5bab74bc379aa9164fd56aa914fb0c6206f0bd9d
Gabriel Mota <gabrielsallesmota@hotmail.com>
2023-05-11 01:22:36 -0300

    Personalizando Admin

--- Alura/Python/Django Web/alura-space/galeria/admin.py
+++ Alura/Python/Django Web/alura-space/galeria/admin.py
@@ -4,9 +4,16 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
+    # list_display = determina colunas menu admin
     list_display = ("id", "nome", "legenda")
+    # links que irão aparecer no menu admin
     list_display_links = ("id", "nome")
+    # adiciona campo busca
     search_fields = ("nome",)
+    # filtrar por categoria
+    list_filter = ("categoria",)
+    # itens por página
+    list_per_pages = 10
 
 
 admin.site.register(Fotografia, ListandoFotografias)
--- Alura/Python/Django Web/alura-space/galeria/models.py
+++ Alura/Python/Django Web/alura-space/galeria/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Fotografia(models.Model):
 
+    ##criação opções (choices)
     OPCOES_CATEGORIA = [
         ("NEBULOSA", "Nebulosa"),
         ("ESTRELA", "Estrela"),
