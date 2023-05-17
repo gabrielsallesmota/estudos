9b098b52efa329e49aa316d3a638febd518da3bf
Gabriel Mota <gabrielsallesmota@hotmail.com>
2023-05-17 02:25:30 -0300

    Buscar / Admin Django

--- Alura/Python/Django Web/alura-space/galeria/admin.py
+++ Alura/Python/Django Web/alura-space/galeria/admin.py
@@ -5,7 +5,7 @@ from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
     # list_display = determina colunas menu admin
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     # links que irão aparecer no menu admin
     list_display_links = ("id", "nome")
     # adiciona campo busca
@@ -14,6 +14,8 @@ class ListandoFotografias(admin.ModelAdmin):
     list_filter = ("categoria",)
     # itens por página
     list_per_pages = 10
+    # tornar o publicada disponível para alteração na pagina de Fotografias 
+    list_editable = ("publicada",)
 
 
 admin.site.register(Fotografia, ListandoFotografias)
