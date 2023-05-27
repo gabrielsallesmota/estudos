75fe1a3f98cdc32dd2529f8e0dff5aaf5ede7eca
Gabriel Mota <gabrielsallesmota@hotmail.com>
2023-05-27 10:02:11 -0300

    Viewset, Urls e requisições GET e POST

--- Alura/Python/API DJango Rest Framework/escola_api_drf/setup/settings.py
+++ Alura/Python/API DJango Rest Framework/escola_api_drf/setup/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'escola_api_drf',
+        'USER': 'admin',
+        'PASSWORD': 'Biel_03101997',
+        'HOST': 'escola-api-drf.cvvmwj4gbhdj.sa-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
