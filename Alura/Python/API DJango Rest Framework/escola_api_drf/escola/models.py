296d96cc728ee3f66f8e013ce3831a011284ae14
Gabriel Mota <gabrielsallesmota@hotmail.com>
2023-06-03 00:34:20 -0300

    Auth Endpoints

--- Alura/Python/API DJango Rest Framework/escola_api_drf/escola/models.py
+++ Alura/Python/API DJango Rest Framework/escola_api_drf/escola/models.py
@@ -24,3 +24,14 @@ class Curso(models.Model):
     def __str__(self):
         return self.descricao
     
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+    ## on_delete=models.CASCADE para quando um aluno for deletado, ele excluí a matricula dele
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
